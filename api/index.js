1b9b0d64242690c94331e51e495b4bf7b3765d97
Abdul <abdou159159159@gmail.com>
2023-09-05 22:04:28 +0100

    server ++

--- api/index.js
+++ api/index.js
@@ -1,31 +1,31 @@
 const express = require('express');
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 const app = express();
-const User = require('./models/User')
+const User = require('./models/User');
 
 app.use(express.json());
 
-const PORT = 3001 || 6001;
-mongoose.connect('mongodb+srv://blog:1GkeKSRMCzcQ1o5C@cluster0.utbkidw.mongodb.net/?retryWrites=true&w=majority').then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port: ${PORT}`);
-    });
-})
-    .catch((error) => {
+const PORT = 3000; // Définition du port que vous souhaitez utiliser
+
+async function startServer() {
+    try {
+        await mongoose.connect('mongodb+srv://blog:1GkeKSRMCzcQ1o5C@cluster0.utbkidw.mongodb.net/?retryWrites=true&w=majority');
+        app.listen(PORT, () => {
+            console.log(`Server running on port: ${PORT}`);
+        });
+    } catch (error) {
         console.log(error.message);
-    });
+    }
+}
+
+startServer();
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
     try {
         const userDoc = await User.create({ username, password });
         res.json(userDoc);
-
     } catch (e) {
-        res.status(400).json(e)
+        res.status(400).json(e);
     }
-    res.json({ requestData: { username, password } });
 });
-
-app.listen(3000);
-
