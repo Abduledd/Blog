e3b7071414f5365e3704d6e7dc753115f1a56fe7
Abdul <abdou159159159@gmail.com>
2023-09-09 15:43:52 +0100

    register done

--- api/index.js
+++ api/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const bcrypt = require('bcryptjs');
+// const cors = require('cors');
 const User = require('./models/User');
+
+
+const app = express();
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(express.json());
 
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+// }));
+
+// const PORT = process.env.PORT || 5000;
+
 const PORT = 3000;
+
 
 async function startServer() {
     try {
@@ -24,7 +37,10 @@ startServer();
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
     try {
-        const userDoc = await User.create({ username, password });
+        const userDoc = await User.create({
+            username,
+            password: bcrypt.hashSync(password, salt),
+        });
         res.json(userDoc);
     } catch (e) {
         console.log('erreur')
