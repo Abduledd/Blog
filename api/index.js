9ce0672ca4db9912fb4d843bf0d8afa1c218941c
Abdul <abdou159159159@gmail.com>
2023-09-09 16:55:12 +0100

    login

--- api/index.js
+++ api/index.js
@@ -3,13 +3,18 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 // const cors = require('cors');
 const User = require('./models/User');
-
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'xnoqdnfodfiouibmqsgroqneofngq'
 
 app.use(express.json());
+app.use(cookieParser());
+
+
 
 // app.use(cors({
 //     origin: 'http://localhost:3000',
@@ -23,7 +28,7 @@ const PORT = 3000;
 async function startServer() {
     try {
         await mongoose.connect('mongodb+srv://blog:1GkeKSRMCzcQ1o5C@cluster0.utbkidw.mongodb.net/?retryWrites=true&w=majority');
-        console.log('server connected')
+        console.log('Server connected to MongoDB')
         app.listen(PORT, () => {
             console.log(`Server running on port: ${PORT}`);
         });
@@ -47,3 +52,35 @@ app.post('/register', async (req, res) => {
         res.status(400).json(e);
     }
 });
+
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const userDoc = await User.findOne({ username });
+    const checkPassword = bcrypt.compareSync(password, userDoc.password);
+    // res.json(checkPassword);
+    if (checkPassword) {
+        //logged in
+        jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json('ok');
+        })
+    } else {
+        res.status(400).json('wrong credentials');
+    }
+});
+
+
+app.get('/profile', (req, res) => {
+    const [token] = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+});
+
+
+
+app.post('/logout', async (req, res) => {
+    res.cookie('token', '').json('ok');
+})
