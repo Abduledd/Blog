a7e63583800d2ed4cb8b1b91415df31f1c45f623
Abdul <abdou159159159@gmail.com>
2023-09-13 21:04:31 +0100

    commit

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -3,28 +3,31 @@ import { useEffect, useState } from "react";
 
 export default function IndexPage() {
   const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    fetch('http://localhost:4000/post')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(posts => {
-        console.log('Posts received:', posts);
-        setPosts(posts);
-      })
-      .catch(error => {
-        console.error('Error fetching posts:', error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch('http://localhost:4000/post')
+  //     .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error('Network response was not ok');
+  //       }
+  //       return response.json();
+  //     })
+  //     .then(posts => {
+  //       console.log('Posts received:', posts);
+  //       setPosts(posts);
+  //     })
+  //     .catch(error => {
+  //       console.error('Error fetching posts:', error);
+  //     });
+  // }, []);
 
-  return (
-    <>
-      {posts.length > 0 && posts.map(post => (
-        <Post {...post} />
-      ))}
-    </>
-  );
+  return
+  <div></div>
+
+  // (
+  //   <>
+  //     {posts.length > 0 && posts.map(post => (
+  //       <Post {...post} />
+  //     ))}
+  //   </>
+  // );
 }
