a1b67994aa3111b660b6222a4e29764eba178626
Abdul <abdou159159159@gmail.com>
2023-09-15 20:18:51 +0100

    commit

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -1,35 +1,53 @@
-import Post from "../Post";
-import { useEffect, useState } from "react";
+// import Post from "../Post";
+// import { useEffect, useState } from "react";
 
-export default function IndexPage() {
-  const [posts, setPosts] = useState([]);
-  // useEffect(() => {
-  //   fetch('http://localhost:4000/post')
-  //     .then(response => {
-  //       if (!response.ok) {
-  //         throw new Error('Network response was not ok');
-  //       }
-  //       return response.json();
-  //     })
-  //     .then(posts => {
-  //       console.log('Posts received:', posts);
-  //       setPosts(posts);
-  //     })
-  //     .catch(error => {
-  //       console.error('Error fetching posts:', error);
-  //     });
-  // }, []);
+// export default function IndexPage() {
+//   const [posts, setPosts] = useState([]);
+//   // useEffect(() => {
+//   //   fetch('http://localhost:4000/post')
+//   //     .then(response => {
+//   //       if (!response.ok) {
+//   //         throw new Error('Network response was not ok');
+//   //       }
+//   //       return response.json();
+//   //     })
+//   //     .then(posts => {
+//   //       console.log('Posts received:', posts);
+//   //       setPosts(posts);
+//   //     })
+//   //     .catch(error => {
+//   //       console.error('Error fetching posts:', error);
+//   //     });
+//   // }, []);
 
-  return
-  <div>
+//   return
+//   <div>
 
-  </div>
+//   </div>
 
-  // (
-  //   <>
-  //     {posts.length > 0 && posts.map(post => (
-  //       <Post {...post} />
-  //     ))}
-  //   </>
-  // );
-}
+//   // (
+//   //   <>
+//   //     {posts.length > 0 && posts.map(post => (
+//   //       <Post {...post} />
+//   //     ))}
+//   //   </>
+//   // );
+// }
+
+import React from "react";
+import PostList from "./PostList";
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Blog</h1>
+      <PostList />
+    </div>
+  );
+};
+
+export default IndexPage;
+
+
+
+
