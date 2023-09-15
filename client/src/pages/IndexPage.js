ea463ab3ee5905f5f76772d533a009bcf132e7ec
Abdul <abdou159159159@gmail.com>
2023-09-15 20:30:25 +0100

    solving problems

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -1,52 +1,54 @@
-// import Post from "../Post";
-// import { useEffect, useState } from "react";
+import Post from "../Post";
+import { useEffect, useState } from "react";
 
-// export default function IndexPage() {
-//   const [posts, setPosts] = useState([]);
-//   // useEffect(() => {
-//   //   fetch('http://localhost:4000/post')
-//   //     .then(response => {
-//   //       if (!response.ok) {
-//   //         throw new Error('Network response was not ok');
-//   //       }
-//   //       return response.json();
-//   //     })
-//   //     .then(posts => {
-//   //       console.log('Posts received:', posts);
-//   //       setPosts(posts);
-//   //     })
-//   //     .catch(error => {
-//   //       console.error('Error fetching posts:', error);
-//   //     });
-//   // }, []);
+export default function IndexPage() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:4000/post')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(posts => {
+        console.log('Posts received:', posts);
+        setPosts(posts);
+      })
+      .catch(error => {
+        console.error('Error fetching posts:', error);
+      });
+  }, []);
 
-//   return
-//   <div>
 
-//   </div>
+  // <div>
 
-//   // (
-//   //   <>
-//   //     {posts.length > 0 && posts.map(post => (
-//   //       <Post {...post} />
-//   //     ))}
-//   //   </>
-//   // );
-// }
-
-import React from "react";
-import PostList from "./PostList";
-
-const IndexPage = () => {
+  // </div>
   return (
     <div>
-      <h1>Welcome to the Blog</h1>
-      <PostList />
+      {posts.length > 0 && posts.map(post => (
+        <Post {...post} />
+      ))}
     </div>
   );
-};
+}
 
-export default IndexPage;
+
+
+
+// import React from "react";
+// import PostList from "./PostList";
+
+// const IndexPage = () => {
+//   return (
+//     <div>
+//       <h1>Welcome to the Blog</h1>
+//       <PostList />
+//     </div>
+//   );
+// };
+
+// export default IndexPage;
 
 
 
